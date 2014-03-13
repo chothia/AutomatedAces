7e5a6dabdceed736647e6b1ccbcb5cc69d4a6b75
John Harris <johnharristhedeveloper@gmail.com>
2014-03-13 10:55:21 -0400

    Added configuration file, CTCP-VERSION handler, fixed configuration

---
+++ config.js
@@ -0,0 +1,26 @@
+var config = {
+	server: "irc.freenode.net",
+	nick: "AcesUpHigh",
+	password: null,
+	userName: "AutomatedAces",
+	realName: "AutomatedAces bot by AcesWayUpHigh",
+	port: 6667,
+	debug: false,
+	showErrors: false,
+	autoRejoin: true,
+	autoConnect: true,
+	channels: ["#roblox"],
+	retryCount: null,
+	retryDelay: 2000,
+	secure: false,
+	selfSigned: false,
+	certExpired: false,
+	floodProtection: true,
+	floodProtectionDelay: 500,
+	sasl: false,
+	stripColors: true,
+	channelPrefixes: "&#",
+	messageSplit: 512
+};
+
+module.exports = config;
