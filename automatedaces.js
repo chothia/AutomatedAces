145639762fd82a158781f6aef22eeeb92289aa0c
John Harris <johnharristhedeveloper@gmail.com>
2014-03-13 13:50:44 -0400

    Added proper package.json, changed main script to load version from package.json

--- automatedaces.js
+++ automatedaces.js
@@ -1,11 +1,9 @@
 var irc = require("irc"), fs = require("fs");
-var aces = {
-	version: "0.0.1"
-};
+var pjson = require("./package.json");
 var config = require("./config");
 var bot = new irc.Client(config.server, config.nick, config);
 bot.addListener("ctcp-version", function(from, to, message){
-	bot.notice(from, "VERSION AutomatedAces " + aces.version);
+	bot.notice(from, "VERSION AutomatedAces " + pjson.version);
 });
 fs.readdirSync("./aces_modules").forEach(function(file){
 	require("./aces_modules/" + file)(bot);
