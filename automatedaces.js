55582dde4976fdf88e420f58619ab7dfb77085bb
John Harris <johnharristhedeveloper@gmail.com>
2014-03-13 10:57:51 -0400

    Added module support

--- automatedaces.js
+++ automatedaces.js
@@ -1,14 +1,12 @@
-var irc = require("irc");
+var irc = require("irc"), fs = require("fs");
 var aces = {
 	version: "0.0.1"
 };
 var config = require("./config");
-var bot = new irc.Client(config.server, config.nick, {
-	channels: config.channels
-});
-bot.addListener("pm", function(from, text, message){
-	
-});
+var bot = new irc.Client(config.server, config.nick, config);
 bot.addListener("ctcp-version", function(from, to, message){
 	bot.notice(from, "VERSION AutomatedAces " + aces.version);
+});
+fs.readdirSync("./aces_modules").forEach(function(file){
+	require("./aces_modules/" + file)(bot);
 });
