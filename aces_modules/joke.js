643cdcd3c9d39cbe287b265b77fa46518efe0d63
Keanu73 <guestone63@gmail.com>
2014-03-20 18:17:32 +0000

    Update joke.js

--- aces_modules/joke.js
+++ aces_modules/joke.js
@@ -3,7 +3,7 @@ module.exports = function(bot){
 		if(message.indexOf('AutomatedAces, know any good jokes?') > -1 || message.indexOf('automatedaces good joke please') > -1){
 			bot.say(to, 'Knock knock!');
 		}else if(message.indexOf('who is there?') > -1 || message.indexOf("who's there?") > -1 || message.indexOf("Who's there?") > -1 || message.indexOf("Who is there?") > -1){
-			bot.say(to, "AutomatedAces");
+			bot.say(to, "AcesWayUpHigh");
 		}else if(message.indexOf('AcesWayUpHigh who?') > -1 || message.indexOf("aceswayuphigh who?") > -1){
 	    		bot.say(to, "AcesWayUpHigh the ROBLOX admin! Didn't you even know that?");
 		}
