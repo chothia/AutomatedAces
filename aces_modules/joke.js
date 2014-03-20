4681c2661b7eba8ca39da45c9d00940d9b4513de
John M Harris, Jr. <johnharristhedeveloper@gmail.com>
2014-03-20 11:11:44 -0400

    Update corny jokes
    Removed two listeners, added a patch for one message invoking all "joke" responses.

--- aces_modules/joke.js
+++ aces_modules/joke.js
@@ -1,27 +1,11 @@
 module.exports = function(bot){
-	bot.addListener('message', function(from, to, message) {
-    if(  message.indexOf('AutomatedAces, know any good jokes?') > -1
-      || message.indexOf('automatedaces good joke please') > -1
-    ) {
-        bot.say(to, 'Knock knock!');
-    }
-});
-bot.addListener('message', function(from, to, message) {
-    if(  message.indexOf('who is there?') > -1
-      || message.indexOf("who's there?") > -1
-      || message.indexOf("Who's there?") > -1
-      || message.indexOf("Who is there?") > -1
-      )
-    {
-        bot.say(to, 'AcesWayUpHigh');
-    }
-});
-bot.addListener('message', function(from, to, message) {
-    if(  message.indexOf('AcesWayUpHigh who?') > -1
-      || message.indexOf("aceswayuphigh who?") > -1
-     )
-    {
-        bot.say(to, "AcesWayUpHigh the ROBLOX admin! Didn't you even know that?");
-    }
-});
+	bot.addListener('message', function(from, to, message){
+		if(message.indexOf('AutomatedAces, know any good jokes?') > -1 || message.indexOf('automatedaces good joke please') > -1){
+			bot.say(to, 'Knock knock!');
+		}else if(message.indexOf('who is there?') > -1 || message.indexOf("who's there?") > -1 || message.indexOf("Who's there?") > -1 || message.indexOf("Who is there?") > -1){
+			bot.say(to, "AutomatedAces");
+		}else if(message.indexOf('AcesWayUpHigh who?') > -1 || message.indexOf("aceswayuphigh who?") > -1){
+	    		bot.say(to, "AcesWayUpHigh the ROBLOX admin! Didn't you even know that?");
+		}
+	});
 };
