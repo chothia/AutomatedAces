ebac9700d9139a26f3f3f1fc5d432f4c6806dd24
John M Harris, Jr. <johnharristhedeveloper@gmail.com>
2014-03-23 00:52:25 -0400

    Update time.js

--- aces_modules/time.js
+++ aces_modules/time.js
@@ -1,5 +1,5 @@
 module.exports = function(bot){
-  bot.addListener('ctcp-time', function(from, to, message){
-    bot.notice(from, 'TIME AutomatedAces ' + pjson.time);
-});
+	bot.addListener("ctcp-time", function(from, to, message){
+		bot.notice(from, "TIME " + new Date().toString());
+	});
 };
